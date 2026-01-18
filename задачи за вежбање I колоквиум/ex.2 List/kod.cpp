ffa8115ef80fddcefc57bce491215c3d0dc17ee7
Sara <aleksovskas28@gmail.com>
2026-01-18 19:34:47 +0100

    Update kod.cpp

--- задачи за вежбање I колоквиум/ex.2 List/kod.cpp
+++ задачи за вежбање I колоквиум/ex.2 List/kod.cpp
@@ -130,33 +130,26 @@ public:
     }
 };
 
-//не менуваш ништо во main
 
+
+
+//не менуваш ништо во main
 int main() {
     ListContainer lc;
     int N;
     cin>>N;
-
     for (int i=0;i<N;i++) {
         int n;
         int niza[100];
-
         cin>>n;
-
         for (int j=0;j<n;j++){
             cin>>niza[j];
-
         }
-
         List l=List(niza,n);
-
         lc.addNewList(l);
     }
-
-
     int testCase;
     cin>>testCase;
-
     if (testCase==1) {
         cout<<"Test case for operator ="<<endl;
         ListContainer lc1;
@@ -167,7 +160,6 @@ int main() {
         cout<<lc1.sum()<<" "<<lc.sum()<<endl;
         lc1.sum();
         lc1.average();
-
     }
     else {
         lc.print();
